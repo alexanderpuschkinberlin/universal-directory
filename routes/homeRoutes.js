c15d0f3f3fe006ffa9df1490955cc0d88a28ca67
pillow <fard.cca@gmail.com>
2022-01-10 21:35:12 +0100

    Fix logged_in

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
 
     res.render("homepage", {
       tags,
-      //   logged_in: req.session.logged_in,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
