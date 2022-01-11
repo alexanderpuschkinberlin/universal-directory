626f2fff8521303bb5f68be487c0339ca6d653b5
pillow <fard.cca@gmail.com>
2022-01-11 18:19:49 +0100

    Fix login in homeRoutes, Add login handlebares

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,22 +1,49 @@
 const router = require("express").Router();
 const { Worker, User, Tag } = require("../models");
-// const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth");
 
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
-    const tagsData = await Tag.findAll();
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
+    });
 
-    // Serialize data so the template can read it
-    const tags = tagsData.map((tag) => tag.get({ plain: true }));
-    // Pass serialized data and session flag into template
+    const users = userData.map((project) => project.get({ plain: true }));
 
     res.render("homepage", {
-      tags,
+      users,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+// // ? check why she get first tags
+// router.get("/", async (req, res) => {
+//   try {
+//     const tagsData = await Tag.findAll();
+
+//     // Serialize data so the template can read it
+//     const tags = tagsData.map((tag) => tag.get({ plain: true }));
+//     // Pass serialized data and session flag into template
+
+//     res.render("homepage", {
+//       tags,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
