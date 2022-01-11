3d3e81ef80d56b8e8f7575f3718fd4ad50ea5633
pillow <fard.cca@gmail.com>
2022-01-11 19:47:12 +0100

    Add login.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const { user } = require("../../models");
+const { User } = require("../../models");
 
 router.post("/login", async (req, res) => {
   try {
-    const userData = await user.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
