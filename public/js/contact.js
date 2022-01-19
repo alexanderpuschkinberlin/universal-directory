eeb251563c80647dd5d41bf82a68870b19bb396a
Zahra Mammadli <zahra.mrj@gmail.com>
2022-01-19 19:55:47 +0100

    Fix contact-us page

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,7 +1,25 @@
-// Responsivess of the navbar
-$(document).ready(function () {
-  $(".sidenav").sidenav();
-});
+// const form = document.querySelector("#contact-form");
+// const email = document.querySelector("#email").value.trim();
+// const sbj = document.querySelector("#subject").value.trim();
+// const msg = document.querySelector("#message").value.trim();
+
+// // Event listener to Send button
+
+// const sendMsg = (e) => {
+//   e.preventDefault();
+//   // Function to send email
+//   email
+//     .send({
+//       SecureToken: "1f96d029-475c-4a1f-850e-8bde99d236cb",
+//       To: email.val(),
+//       From: "theboaringapp@gmail.com",
+//       Subject: sbj.val(),
+//       Body: msg.val(),
+//     })
+//     .then((message) => alert(message));
+// };
+
+// document.querySelector("#sendBtn").addEventListener("click", sendMsg);
 
 const form = $("#contact-form");
 const email = $("#email");
